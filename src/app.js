c62a03ba4a0093a4988454f1e856cd1cd51db235
Make310 <luisfmarquez1999@gmail.com>
2022-11-01 20:29:28 -0500

    se inicializa la aplicacion

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Se inicio la aplicacion");
+});
+
+app.set("port", process.env.PORT);
+
+module.exports = app;
---
+++ src/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const URI = process.env.MONGODB_URI;
+
+mongoose.connect(URI);
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("Conexion a la base de datos realizada con exito!!");
+});
+
+module.exports = connection;
